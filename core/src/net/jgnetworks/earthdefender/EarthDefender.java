32b17426f298a8862138956e20a8880bbd8709a2
Justin Guarino <justin@jgnetworks.net>
2015-12-27 14:27:39 -0800

    Adding asteroid and ship animations

--- core/src/net/jgnetworks/earthdefender/EarthDefender.java
+++ core/src/net/jgnetworks/earthdefender/EarthDefender.java
@@ -7,7 +7,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -19,13 +23,21 @@ import com.badlogic.gdx.utils.TimeUtils;
 
 
 public class EarthDefender extends ApplicationAdapter {
+	//Various game mechanic objects
 	private SpriteBatch batch;
-	private Texture shipTexture;
-	private Texture asteroidTexture;
-	private Music bgm;
+	private float elapsedTime = 0;
 	private OrthographicCamera camera;
-	private Rectangle player;
 	private Vector3 touchPos;
+	private Music bgm;
+	
+	//Player specific objects
+	private TextureAtlas shipTextureAtlas;
+	private Animation shipIdleAnimation;
+	private Rectangle player;
+	
+	//Enemy specific objects
+	private TextureAtlas asteroidTextureAtlas;
+	private Animation asteroidIdleAnimation;
 	private Array<Rectangle> asteroids;
 	private long lastEnemySpawn;
 	
@@ -36,21 +48,25 @@ public class EarthDefender extends ApplicationAdapter {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 480, 800);
 		touchPos = new Vector3();
-		asteroids = new Array<Rectangle>();
 		
-		shipTexture = new Texture(Gdx.files.internal("player/ship1.png"));
-		asteroidTexture = new Texture(Gdx.files.internal("enemy/asteroid1.png"));
-		bgm = Gdx.audio.newMusic(Gdx.files.internal("EarthDefenderFull.mp3"));
-		
+		//Create and animate player
+		shipTextureAtlas = new TextureAtlas(Gdx.files.internal("player/ship/shippack/shippack.atlas"));
+		shipIdleAnimation = new Animation (1/6f, shipTextureAtlas.getRegions());
 		player = new Rectangle();
-		player.x = 480/2-64/2;
-		player.y = 20;
 		player.width = 64;
 		player.height = 64;
+		player.x = 480/2-player.width/2;
+		player.y = 20;
 		
+		
+		//Create and animate enemies
+		asteroids = new Array<Rectangle>();
+		asteroidTextureAtlas = new TextureAtlas(Gdx.files.internal("enemy/asteroid/idleanim/asteroidanimpack.atlas"));
+		asteroidIdleAnimation = new Animation(1/4f, asteroidTextureAtlas.getRegions());
 		spawnEnemy();
 		lastEnemySpawn = TimeUtils.nanoTime();
 		
+		bgm = Gdx.audio.newMusic(Gdx.files.internal("EarthDefenderFull.mp3"));
 		bgm.setLooping(true);
 		bgm.play();
 	}
@@ -61,9 +77,10 @@ public class EarthDefender extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(shipTexture, player.x, player.y, 64, 64);
+		elapsedTime+=Gdx.graphics.getDeltaTime();
+		batch.draw(shipIdleAnimation.getKeyFrame(elapsedTime, true), player.x, player.y, player.width, player.height);
 		for(Rectangle asteroid : asteroids){
-			batch.draw(asteroidTexture, asteroid.x, asteroid.y, 64, 64);
+			batch.draw(asteroidIdleAnimation.getKeyFrame(elapsedTime, true), asteroid.x, asteroid.y, 64, 64);
 		}
 		batch.end();
 		
@@ -71,20 +88,20 @@ public class EarthDefender extends ApplicationAdapter {
 		if(Gdx.input.isTouched()) {
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
-			player.x = touchPos.x - 64/2;
+			player.x = touchPos.x - player.width/2;
 		}
 		
 		//Keyboard directional key movement logic
 		if(Gdx.input.isKeyPressed(Keys.LEFT)) 
-			player.x -= 200 * Gdx.graphics.getDeltaTime();
+			player.x -= 300 * Gdx.graphics.getDeltaTime();
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) 
-			player.x += 200 * Gdx.graphics.getDeltaTime();
+			player.x += 300 * Gdx.graphics.getDeltaTime();
 		
 		//Keep player in bounds of screen
 		if(player.x < 0)
 			player.x = 0;
-		if(player.x>480-64)
-			player.x = 480-64;
+		if(player.x>480-player.width)
+			player.x = 480-player.width;
 		
 		if(TimeUtils.nanoTime() - lastEnemySpawn > 1000000000)
 			spawnEnemy();
@@ -96,7 +113,7 @@ public class EarthDefender extends ApplicationAdapter {
 			if(asteroid.y + 64 < 0)
 				iter.remove();
 			if(asteroid.overlaps(player)){
-				iter.remove();
+				//destroy player
 			}
 		}
 	}
@@ -113,8 +130,8 @@ public class EarthDefender extends ApplicationAdapter {
 	
 	@Override
 	public void dispose() {
-		shipTexture.dispose();
-		asteroidTexture.dispose();
+		shipTextureAtlas.dispose();
+		asteroidTextureAtlas.dispose();
 		bgm.dispose();
 		batch.dispose();
 	}
